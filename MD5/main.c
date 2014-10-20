9e42970453fc357ff394bcd4c8b3dd14d9570c81
serializedowen <wjh199455@gmail.com>
2014-10-20 17:03:06 -0400

    Finished MD5.h

--- MD5/main.c
+++ MD5/main.c
@@ -6,10 +6,38 @@
 //  Copyright (c) 2014 OwenInc. All rights reserved.
 //
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "MD5.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+static char * progname;
+
+
+int main(int argc, char ** argv) {
+    
+    progname = argv[0];
+    
+    int c, status = 0;
+    int fileflag = 0;
+    while ((c = getopt(argc, argv, "f") != EOF)) {
+        switch (c) {
+            case 'f':
+                fileflag = 1;
+                break;
+            case '?':
+                status = 1;
+                break;
+            default:
+                break;
+        }
+        if(status || optind + 1 != argc){
+            fprintf(stderr, "usage: %s [-b count] [name]\n", progname);
+        }
+    }
+    
+    
+    
     return 0;
 }
